8f6968525c4f403d325975c495f858d2cce4ab5f
Zebadiah Miller <Zebadiahmiller@homtial.com>
2019-06-04 23:29:24 -0700

    added some css

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -8,6 +8,7 @@ const selectedWordArray = selectedWord.split('');
 let blanks = 0;
 let wrongLetter = [];
 let wrongGuess = [];
+let keyPresses = [];
 let wins = 0;
 let losses = 0;
 let guessesLeft = 9;
@@ -19,46 +20,56 @@ var underscoreString = underscoreArray.join('');
 document.getElementById('currentword').innerHTML = underscoreString;
 
 document.addEventListener('keyup', function (event) {
-    var key = event.key
-   
+    var key = event.key;
+
+
+
+
     for (let i = 0; i < selectedWordArray.length; i++) {
         if (selectedWordArray[i] === key) {
             underscoreArray[i] = key
         }
         else {
-            
+
             guessesLeft--;
         };
-        if (selectedWordArray[i] !== key){
-            wrongGuess = key;
-           document.getElementById('lettersguessed').innerHTML = wrongGuess;
+        if (selectedWordArray[i] !== key) {
+            console.log(`is ${selectedWordArray[i]} !== ${key}?`);
+            wrongGuess.push(key);
+            // document.getElementById('lettersguessed').innerHTML = wrongGuess;
 
         }
+
+
+
+
     };
+  
+//add in a line that just logs key presses and compare to the guessed letters outside of a loop
 
     //win conditions
-  if (underscoreArray.toString() == selectedWordArray.toString()){
-      wins++;
-      document.getElementById('wins').innerHTML =wins;
-      
-  }
-  if (guessesLeft < 1){
-      losses++;
-      document.getElementById('losses').innerHTML = losses;
+    if (underscoreArray.toString() == selectedWordArray.toString()) {
+        wins++;
+        document.getElementById('wins').innerHTML = wins;
 
-  }
+    }
+    if (guessesLeft < 1) {
+        losses++;
+        document.getElementById('losses').innerHTML = losses;
 
-    
-    
+    }
+
+
+
     console.log(underscoreArray)
     underscoreString = underscoreArray.join('');
     document.getElementById('currentword').innerHTML = underscoreString;
     underscoreString = wrongLetter.join('');
-    
+
     document.getElementById('guessesleft').innerHTML = guessesLeft;
 
 });
-console.log(wrongLetter)
+console.log(keyPresses)
 
 
 
