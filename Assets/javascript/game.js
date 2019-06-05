3c083f84c953537a4845af09dc6b668722e0de40
Zebadiah Miller <Zebadiahmiller@homtial.com>
2019-06-04 17:00:42 -0700

    working counters for win

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -6,8 +6,10 @@ const selectedWord = characters[Math.floor(Math.random() * characters.length)];
 console.log(selectedWord)
 const selectedWordArray = selectedWord.split('');
 let blanks = 0;
-const rightWrong = [];
+let wrongLetter = [];
 const wrongGuess = [];
+let wins = 0
+let guessesLeft = 9;
 
 // array full of underscores
 var underscoreArray = [...Array(selectedWord.length).keys()].fill('_ ');
@@ -18,20 +20,33 @@ document.getElementById('currentword').innerHTML = underscoreString;
 document.addEventListener('keyup', function (event) {
     var key = event.key
    
-    for (i = 0; i < selectedWordArray.length; i++) {
+    for (let i = 0; i < selectedWordArray.length; i++) {
         if (selectedWordArray[i] === key) {
             underscoreArray[i] = key
-        };
-        if (selectedWordArray[i] !== key){
-            document.getElementById('guesses').innerHTML = underscoreString;
-
-        };
+        }
+        else {
+            
+            guessesLeft--;
+        }
     }
+
+    //win conditions
+  if (underscoreArray.toString() == selectedWordArray.toString()){
+      wins++;
+      document.getElementById('wins').innerHTML =wins;
+      
+  }
+    
+    
     console.log(underscoreArray)
     underscoreString = underscoreArray.join('');
     document.getElementById('currentword').innerHTML = underscoreString;
+    underscoreString = wrongLetter.join('');
+    
+    document.getElementById('guessesleft').innerHTML = guessesLeft;
 
 });
+console.log(wrongLetter)
 
 
 
